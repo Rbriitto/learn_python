3f044bcc03aaff5d9005711399f5b2e1251437ef
Rbriitto <66185384+Rbriitto@users.noreply.github.com>
2025-08-21 14:31:50 +0000

    exercicios

--- exerciciosrange.py
+++ exerciciosrange.py
@@ -1,3 +1,6 @@
+# class range(stop)
+# class range(start, stop[, step])
+
 # Exercício 1: Contagem Simples
 # Imprima os números de 1 a 10 usando um loop for e range().
 
@@ -13,5 +16,26 @@
 # for i in range (10):
 #     print (i)
 
-# 
+# Exercício 3: Contagem Regressiva
+# Imprima os números de 10 a 1 em ordem decrescente.
+# for i in range (10,0,-1):
+#     print(i)
+
+
+# Exercício 4: Números Pares
+# Imprima todos os números pares entre 0 e 20 (inclusive).
+# for i in range (2,21,2):
+#     print(i)
+
+# Exercício 5: Múltiplos de 5
+# Imprima todos os múltiplos de 5 entre 10 e 50 (inclusive).
+# for i in range (10,51,5):
+#     print(i)
+
+# Exercício 6: Somando Números
+# Calcule e imprima a soma de todos os números de 1 a 100.
+# soma = 0
+# for i in range (1,101):
+#     soma += i 
+# print(soma)
 
