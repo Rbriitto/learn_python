a5453d83be28190ccd87f1a30c2c234a6573d39a
Rbriitto <66185384+Rbriitto@users.noreply.github.com>
2025-08-23 13:31:29 +0000

    learn

---
+++ 2 - Modulos Nativos/main.py
@@ -0,0 +1,15 @@
+# Programa Principal
+
+import math_operations
+from math_operations import multiply, divide
+
+print(math_operations.sum(5, 3))
+print(math_operations.subtract(5, 3))
+print(multiply(5, 3))
+print(divide(5, 3))
+
+import string_utils
+
+print(string_utils.capitalize("\n\nhello"))
+print(string_utils.count("verificando"))
+print(string_utils.reverse_string("python"))
---
+++ 2 - Modulos Nativos/math_operations.py
@@ -0,0 +1,17 @@
+# Operações matematicas 
+
+def sum(x , y):
+    return x + y
+
+def subtract(x , y):
+    return x - y
+
+def multiply (x, y):
+    return x * y
+
+def divide (x , y):
+    if y != 0:
+        return x / y
+    else:
+        raise ValueError("Erro!")
+
